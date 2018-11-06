f4ca18121952334587454915842047f21d4156f1
Gabriela Suriano <surianogabriela@gmail.com>
2018-11-06 08:24:07 +0000

    automatically backed up by learn

--- src/components/BandInput.js
+++ src/components/BandInput.js
@@ -4,6 +4,25 @@ import { connect } from 'react-redux';
 
 
 class BandInput extends Component {
+    constructor() {
+  super()
+  this.state = {
+    name: ""
+  }
+  }
+  handleOnChange = event => {
+  this.setState({
+    name: event.target.value
+  })
+  }
+  handleSubmit = event => {
+  event.preventDefault()
+  this.props.addBand(this.state)
+  this.setState({
+    name: ""
+  })
+  }
+  
   render() {
     return(
       <div>
